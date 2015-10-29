a511bc69bf1477f0de93febe68fab3c136df6181
weihf <weihfjob@163.com>
2015-10-29 16:57:36 +0800

    e

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 $(function() {
-	
+	//首先将#back-to-top隐藏
+	$("#backTop").hide();
+	//当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+	$(function() {
+		$(window).scroll(function() {
+			if ($(window).scrollTop() > 100) {
+				$("#backTop").fadeIn(1500);
+			} else {
+				$("#backTop").fadeOut(1000);
+			}
+		});
+		//当点击跳转链接后，回到页面顶部位置
+		$("#backTop").click(function() {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 1000);
+			return false;
+		});
+	});
 })
